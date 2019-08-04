a322b51198daf6aa551ec08f56f0b4578284ce26
Emily G <aspentree79@gmail.com>
2019-08-04 19:02:17 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,6 @@
-# Your code here
+require."pry"
 def meal_choice(veg1, veg2, protein="meat")
-# puts "What a nutritious meal!"
+puts "What a nutritious meal!"
 puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
